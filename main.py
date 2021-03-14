8ec158bc79b0fc33d90abe031d9999a1b05f50b8
Vyacheslav <krut.slava@inbox.ru>
2021-03-14 14:17:30 +0600

    Edited main.py, Added help handler, comments, edited start handler, added logging

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 from config import BOT_TOKEN, ADMIN_ID
 
+logging.basicConfig(level=logging.INFO)
+
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot)
 
@@ -9,17 +11,24 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     """
-    This handler will be called when user sends `/start` or `/help` command
+    This handler will be called when user sends `/start` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
-    print(message.text)
+    await message.reply("Hi!\nI'm QuotesBot!")
+
+
+@dp.message_handler(commands=['help'])
+async def send_help(message: types.Message):
+    """
+    This handler will be called when user sends '/help' command
+    """
+    await message.reply("I almost do nothing but I'll be a good bot soon")
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
-    print(message.text)
+    """
+    This is echo handler
+    """
     await message.answer(message.text)
 
 
