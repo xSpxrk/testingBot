ddd9c8f0219a767253bc97c28ad97fa3f2352196
Vyacheslav <krut.slava@inbox.ru>
2021-03-17 13:47:10 +0600

    Added file SQL.py

---
+++ SQL.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+class SQL:
+    def __init__(self, database_file):
+        self.connection = sqlite3.connect(database_file)
+        self.cursor = self.connection.cursor()
+
+    def close(self):
+        self.connection.close()
+
+    def load_file(self, file_id):
+        with self.connection:
+            self.cursor.execute("INSERT INTO 'Pictures' ('File_id') VALUES (?)", (file_id,))
+
+    def get_pic(self, tag):
+        with self.connection:
+            return self.cursor.execute("SELECT File_id FROM Pictures").fetchall()
